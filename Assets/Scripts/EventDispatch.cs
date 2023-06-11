578a467a139a396c076c06cd14d0960d3dc7ba8a
Christian M. Schrader <cmschrader@wpi.edu>
2023-06-11 01:19:42 -0400

    Courtine Dispatch

--- Assets/Scripts/EventDispatch.cs
+++ Assets/Scripts/EventDispatch.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
+using Random=System.Random;
+
 
 public class Dispatch 
 {
@@ -49,25 +52,65 @@ public class EventDispatch : MonoBehaviour
         current = this;
     }
 
-    public Queue<Dispatch> dispatchQueue;
+    public float averageWait = 40;
+    public float waitRange = 20;
+    private float nextWait;
+    private float lastTime = 0f;
+    
+    private Random rng;
 
     [SerializeField] private UnityEvent normalize;
     [SerializeField] private UnityEvent exersise;
 
     void Start() {
-        dispatchQueue = new Queue<Dispatch>();
-        dispatchQueue.Enqueue(new Dispatch(exersise, 5));
-        dispatchQueue.Enqueue(new Dispatch(normalize, 10));
+        rng = new Random();
+        SetWait();
+        // StartCoroutine(RunBPMEvent(500, 3, 5));
+
+        // dispatchQueue = new Queue<Dispatch>();
+        // dispatchQueue.Enqueue(new Dispatch(exersise, 5));
+        // dispatchQueue.Enqueue(new Dispatch(normalize, 10));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (dispatchQueue.Count > 0) {
-            if (dispatchQueue.Peek().Invoke(Time.time)) {
-                dispatchQueue.Dequeue();
+        if (Time.time - lastTime > nextWait) {
+            SetWait();
+            
+            if (rng.NextDouble() > 0.5) {
+                StartCoroutine(RunBPMEvent(500, 3, 5));
+            } else {
+                StartCoroutine(RunStrengthEvent(500, 3, 5));
             }
         }
+    }
+
+    IEnumerator RunBPMEvent(float targetBPM, float warning, float length) {
+        Debug.Log("BPM Event Coming");
+        yield return new WaitForSeconds(warning);
+        SetBPM(targetBPM);
+        Debug.Log("BPM Event Started");
+        yield return new WaitForSeconds(length);
+        SetNormalize();
+        Debug.Log("BPM Event Ended");
+    }
+
+    IEnumerator RunStrengthEvent(float newStrength, float warning, float length) {
+        Debug.Log("Strength Event Coming");
+        yield return new WaitForSeconds(warning);
+        SetBeatStrength(newStrength);
+        Debug.Log("Strength Event Started");
+        yield return new WaitForSeconds(length);
+        SetNormalize();
+        Debug.Log("Strength Event Ended");
+    }
+
+
+    private void SetWait() {
+        lastTime = Time.time;
+        nextWait = averageWait + waitRange * 2f * ((float) rng.NextDouble() - 0.5f);
+        Debug.Log("Next wait is " + nextWait);
     }
 
     /* ----------------------------------------------------------------------------------------- */
